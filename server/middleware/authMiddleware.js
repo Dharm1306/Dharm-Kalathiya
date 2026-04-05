fd90669559be9a2faf4f9ab7c3e496af59fc273a
Dharm1306 <dharmkalathiya2006@gamil.com>
2026-04-05 09:45:38 +0530

    update server code

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -4,10 +4,21 @@ import User from "../models/User.js";
 export const protect = async (req, res, next) => {
   const { userId } = req.auth;
   if (!userId) {
-    res.json({ success: false, message: "not authenticated" });
-  } else {
-    const user = await User.findById(userId);
-    req.user = user;
-    next();
+    return res.json({ success: false, message: "not authenticated" });
   }
+
+  let user = await User.findById(userId);
+  if (!user) {
+    user = await User.create({
+      _id: userId,
+      username: `user-${userId.slice(0, 6)}`,
+      email: `no-reply+${userId}@quickstay.local`,
+      image: 'https://ui-avatars.com/api/?name=Guest&background=FF6B35&color=fff',
+      role: 'user',
+      recentSearchedCities: [],
+    });
+  }
+
+  req.user = user;
+  next();
 };
